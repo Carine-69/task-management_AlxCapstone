3aebe6123a029fc86652115eb28af286cde31354
Carine-69 <umugabecarine@gmail.com>
2025-01-10 11:40:23 +0200

    making app owrk

--- TaskManager/taskManager/urls.py
+++ TaskManager/taskManager/urls.py
@@ -1,7 +1,10 @@
-from django.url import path include
-from rest_framework import DefaultRouter
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from .views import taskViewSet
 
-router = DefaultRouter
-router.register(r'tasks', taskViewSet)
-urlpatterns = ['api/'include(router.urls),]
+router = DefaultRouter()
+router.register(r'tasks', taskViewSet) 
+
+urlpatterns = [
+    path('api/', include(router.urls)),
+]
--- TaskManager/taskManager/views.py
+++ TaskManager/taskManager/views.py
@@ -1,26 +1,17 @@
 from django.contrib.auth import get_user_model
-from .models import taskManager
-from .Serializer import taskSerializer
-from rest_framework import Viewsets
+from .models import taskManager  
+from .Serializer import taskSerializer  
+from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+
 # Create your views here.
+class taskViewSet(viewsets.ModelViewSet):  
+    serializer_class = taskSerializer  
+    permission_classes = [IsAuthenticated]
 
-class taskViewSet(ViewSets.ModelViewSet):
-	serializer_class = taskSerializer
-	permission_classes = [IsAuthenticated]
-
-#this is to use ViewSet to handle CRUD operations
-	def get_self_quaryset(self):
-		user = self.request.user
-		return taskMnager.objects.filter(user=user)
-
-#this is to make sure that the user only sees and manage his/her own tasks only
-
-	def save_creation(self):
-		serializer.save(user=self.request.user)
-
-#this is to automatically save user field 
-
-
-
-
+    def get_queryset(self):
+        user = self.request.user
+        return taskManager.objects.filter(user=user)  
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)  
